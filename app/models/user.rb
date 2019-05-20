77c8c71e39da7b0f41f89bccfc94cb830f67364b
mathilde ferrera <mathilde.ferrera9@gmail.com>
2019-05-20 19:05:50 +0200

    kid new form in progress

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :bookings
-  has_many :kids
+  has_many :kids, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true
 end
