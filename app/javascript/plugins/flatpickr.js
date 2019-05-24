41adcede41d65da63e895d869f8ac884bb400dfa
Laura Quecan <lauraquecan@icloud.com>
2019-05-24 12:23:06 +0200

    Booking total calculator

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -3,7 +3,17 @@ import "flatpickr/dist/themes/airbnb.css" // Note this is important!
 import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
 
 flatpickr("#range_start", {
-    mode: "range"
+    mode: "range",
+    onClose: function(selectedDates, dateStr, instance) {
+    	const numberDays = (selectedDates[1] - selectedDates[0])/60/60/24/1000;
+    	const priceStr = document.querySelector('t').innerHTML;
+    	const price = Number.parseInt(priceStr)
+    	
+    	const bookingTotal = document.getElementById('booking-total');
+
+    	bookingTotal.insertAdjacentHTML('beforeend', `<p>Nbr of days: ${numberDays} </p>`);
+    	bookingTotal.insertAdjacentHTML('beforeend', `<p>Total Price: ${numberDays * price} </p>`);
+    }
 })
 
 
